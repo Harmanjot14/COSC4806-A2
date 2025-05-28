e7c47d39b923edded2a7a4d12d3f4ec8af437ea9
Harmanjot14 <jotkharman@gmail.com>
2025-05-28 23:22:55 +0000

    Created Config file and added database info

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+  define('DB_HOST', 'q8wg0.h.filess.io');
+  define('DB_USER', 'COSC4806_twosheepbe');
+  define('DB_PASS', $_ENV['DB_PASS']);
+  define('DB_DATABASE', 'COSC4806_twosheepbe');
+  define('DB_PORT', '61000');
+
+?>
