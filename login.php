3b2b3a0d7d9a4c2be49904ac47b61da75e548146
Harmanjot14 <jotkharman@gmail.com>
2025-05-30 20:21:09 +0000

    Copied login file from Assignment 1

---
+++ login.php
@@ -0,0 +1,35 @@
+<?php
+
+session_start();
+
+//if failed attempt than displaying it at login page
+if(isset($_SESSION['failed_attempts'])){
+  echo "This is unsuccessful attempt number ". $_SESSION['failed_attempts'];
+}
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Login</title>
+  </head>
+
+  <body>
+
+    <h1>Login Forum</h1>
+    <form action="/validate.php" method="post">
+      <label for="username">Username:</label>
+      <br>
+      <input type="text" id="username" name="username">
+      <br>
+      <label for="password">Password:</label>
+      <br>
+      <input type="password" id="password" name="password">
+      <br><br>
+      <input type="submit" value="Submit">
+      </form>
+
+
+  </body>
+
+</html>
