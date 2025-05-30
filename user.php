a45198aa4b6405ab4ccd261f2a962a484ce7f297
Harmanjot14 <jotkharman@gmail.com>
2025-05-30 20:27:13 +0000

    Added function in user file
    get_user to get user by its username

--- user.php
+++ user.php
@@ -11,6 +11,14 @@
       return $rows;
     }
 
+    /* get user by username */
+    public function get_user($username){
+      $db = db_connect();
+      $statement = $db->prepare("select * from users where username = :username;");
+      $statement->execute(['username' => $username]);
+      $rows = $statement->fetch(PDO::FETCH_ASSOC);
+    }
+
     public function create_user($username, $password){
       $db = db_connect();
       /*to check if username already exists*/
@@ -20,15 +28,13 @@
       if ($rows){
         return "Username already exists";
       }
-
       /*validate password is at least 10 characters*/
       if (strlen($password) < 10 || !preg_match('/[A-Z]/', $password) || !preg_match('/[a-z]/', $password))
         return "Password must be at least 10 characters, contain at least one uppercase letter and one lowercase letter";
 
-    
       /*hashed password*/
       $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-    
+  
       /*create a new user*/
       $statement = $db->prepare("INSERT INTO users (username, password) VALUES (:username, :password);");
       $statement->execute(['username' => $username, 'password' => $hashedPassword]);
